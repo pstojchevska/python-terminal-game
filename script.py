6e11356dba1434b6cf05426002e5ca6ebbaadc50
Paula Stojchevska <60014821+pstojchevska@users.noreply.github.com>
2024-09-01 23:59:41 +0200

    Add random ship placement function

--- script.py
+++ script.py
@@ -1,3 +1,5 @@
+import random
+
 def print_board(board):
     for row in board:
         print(" ".join(row))
@@ -7,3 +9,12 @@ board_size = 5
 board = [["O"] * board_size for _ in range(board_size)]
 
 print_board(board)
+
+def place_ship(board):
+    row = random.randint(0, board_size - 1)
+    col = random.randint(0, board_size - 1)
+    board[row][col] = "S"
+    return (row, col)
+
+ship_location = place_ship(board)
+
